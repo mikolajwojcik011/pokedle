b1f3557bf1e320435a6734a75ffcaf42b1a4a476
Mikołaj <mikolajwojcik011@gmail.com>
2024-09-02 15:00:20 +0200

    ui setup

--- src/app/components/classic/classic.component.ts
+++ src/app/components/classic/classic.component.ts
@@ -8,5 +8,89 @@ import { Component } from '@angular/core';
   styleUrl: './classic.component.css'
 })
 export class ClassicComponent {
-
+  guesses: {
+    pokemon: string,
+    type1: string,
+    type2: string | null,
+    habitat: string,
+    evolutionStage: number,
+    height: number,
+    weight: number
+  }[] = [
+    {
+      pokemon: 'bulbasaur',
+      type1: 'grass',
+      type2: 'poison',
+      habitat: 'grassland',
+      evolutionStage: 1,
+      height: 7,
+      weight: 69
+    },
+    {
+      pokemon: 'charmander',
+      type1: 'fire',
+      type2: null,
+      habitat: 'mountain',
+      evolutionStage: 1,
+      height: 6,
+      weight: 85
+    },
+    {
+      pokemon: 'squirtle',
+      type1: 'water',
+      type2: null,
+      habitat: 'waters-edge',
+      evolutionStage: 1,
+      height: 5,
+      weight: 90
+    },{
+      pokemon: 'squirtle',
+      type1: 'water',
+      type2: null,
+      habitat: 'waters-edge',
+      evolutionStage: 1,
+      height: 5,
+      weight: 90
+    },{
+      pokemon: 'squirtle',
+      type1: 'water',
+      type2: null,
+      habitat: 'waters-edge',
+      evolutionStage: 1,
+      height: 5,
+      weight: 90
+    },{
+      pokemon: 'squirtle',
+      type1: 'water',
+      type2: null,
+      habitat: 'waters-edge',
+      evolutionStage: 1,
+      height: 5,
+      weight: 90
+    },{
+      pokemon: 'squirtle',
+      type1: 'water',
+      type2: null,
+      habitat: 'waters-edge',
+      evolutionStage: 1,
+      height: 5,
+      weight: 90
+    },{
+      pokemon: 'squirtle',
+      type1: 'water',
+      type2: null,
+      habitat: 'waters-edge',
+      evolutionStage: 1,
+      height: 5,
+      weight: 90
+    },{
+      pokemon: 'squirtle',
+      type1: 'water',
+      type2: null,
+      habitat: 'waters-edge',
+      evolutionStage: 1,
+      height: 5,
+      weight: 90
+    },
+  ];
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
         'bckg-light': '#F8F6D7',
         'green-gray': '#4F5F47',
         'light-green': '#76E39E',
-        'blue': '#294E68'
+        'blue': '#294E68',
+        'gold': '#CAA746',
+        'pink': '#ECD9EF',
+        'orange': '#F36445'
       },
       fontFamily: {
         'display': ['Josefin Sans'],
